758b3f34c376dcd90e7e9873e9c91ddaf6acd251
PrabhatYadav-27 <prabhaty2727@gmail.com>
2025-04-16 23:44:01 +0530

    finding the headline of the given url

---
+++ scrape.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+
+(async ()=>{
+    const url = process.env.SCRAPE_URL || 'https://example.com'
+    console.log(`Using url:  ${url}`); //log the url being used
+
+    const browser = await puppeteer.launch({
+        headles: true,
+        args: ['--no-sandbox']
+    });
+
+    //open in new page
+
+    const page = await  browser.newPage();
+
+    try{
+        await page.goto(url,{waitUntil: 'domcontentloaded'});
+
+    } catch(err){
+        console.log(`Failed to load page: ${url}`, err);
+        await browser.close();
+        return;
+    }
+
+    const data = await page.evaluate(() => {
+        const title = document.title;
+        let heading = document.querySelector('h1')?.innerText || 'No Heading Found'
+    })
+})
