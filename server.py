e4f3f02cd9aa9999a92874e6d3516e1f749b5da6
PrabhatYadav-27 <prabhaty2727@gmail.com>
2025-04-17 17:26:30 +0530

    done with routing and hosting the flask server

--- server.py
+++ server.py
@@ -1,2 +1,14 @@
 from flask import Flask, jsonify
 import json
+app = Flask(__name__)
+
+# making the route to open the scrape data from url
+@app.route("/")
+def home():
+    with open("scraped_data.json") as f:
+        data = json.load(f)
+    return jsonify(data)
+
+
+# hosting on port 5000
+app.run(host="0.0.0.0", port=5000)
